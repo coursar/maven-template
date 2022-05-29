75e14e11c59bf03995ae660cc01de4659cd9519a
student <student@localhost>
2022-05-29 17:49:39 +0300

    faker removed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,5 @@ import com.github.javafaker.Faker;
 
 public class Main {
   public static void main(String[] args) {
-    final Faker faker = new Faker();
-    final String card = faker.finance().creditCard();
-    System.out.println("card = " + card);
   }
 }
