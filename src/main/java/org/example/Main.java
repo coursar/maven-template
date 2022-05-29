29c127e0defcaeaedc73322fce8ae1c405c7b42d
student <student@localhost>
2022-05-29 17:40:04 +0300

    uber jar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import com.github.javafaker.Faker;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hello world");
+    final Faker faker = new Faker();
+    final String card = faker.finance().creditCard();
+    System.out.println("card = " + card);
   }
 }
